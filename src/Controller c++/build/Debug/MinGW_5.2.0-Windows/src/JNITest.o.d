0fc86f4c406f3c66bd85ef73a23b50087157c5be
Henk Mollema <henkmollema@gmail.com>
2015-11-12 11:56:23 +0100

    Update .gitignore

--- src/Controller c++/build/Debug/MinGW_5.2.0-Windows/src/JNITest.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/MinGW_5.2.0-Windows/src/JNITest.o: src/JNITest.cpp \
- C:/Program\ Files/Java/jdk1.8.0_65/include/jni.h \
- C:/Program\ Files/Java/jdk1.8.0_65/include/win32/jni_md.h src/JNITest.h
-
-C:/Program\ Files/Java/jdk1.8.0_65/include/jni.h:
-
-C:/Program\ Files/Java/jdk1.8.0_65/include/win32/jni_md.h:
-
-src/JNITest.h:

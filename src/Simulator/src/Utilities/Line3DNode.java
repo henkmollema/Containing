b93af5bd839a9926505f04994f7fdf3147ac418d
mphjens <jensvossnack@gmail.com>
2015-11-14 19:02:28 +0100

    Added the line renderer
    Example usage:
    
    List<Line3DNode> points = new ArrayList<Line3DNode>();
    Line3DNode tmp = new Line3DNode();
    
    tmp.position = new Vector3f(0,0,0);
    points.add(tmp);
    
    tmp = new Line3DNode();
    tmp.position = new Vector3f(10f,0f,0f);
    points.add(tmp);
    
    tmp = new Line3DNode();
    tmp.position = new Vector3f(30f,10f,0f);
    points.add(tmp);
    
    tmp = new Line3DNode();
    tmp.position = new Vector3f(40f,0f,0f);
    points.add(tmp);
    
    line = new Line3D(points, this.getCamera(), mat);
    rootNode.attachChild(line);

---
+++ src/Simulator/src/Utilities/Line3DNode.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+
+/**
+ * 
+ * @author Jens
+ */
+public class Line3DNode {
+    public Vector3f position    = Vector3f.ZERO;
+    public float width          = 1f;
+    public ColorRGBA color      = ColorRGBA.White;
+    
+}

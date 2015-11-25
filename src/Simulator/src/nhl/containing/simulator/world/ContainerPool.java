71734198b72767124eed071a374415253d907fc2
hoekkii <hoek1403@student.nhl.nl>
2015-11-25 20:56:47 +0100

    Simple Container Pooling

---
+++ src/Simulator/src/nhl/containing/simulator/world/ContainerPool.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nhl.containing.simulator.world;
+
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import nhl.containing.simulator.simulation.Transform;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author sietse
+ */
+public class ContainerPool {
+    private static List<Transform> m_activePool = new ArrayList<Transform>();
+    private static List<Transform> m_inactivePool = new ArrayList<Transform>();
+    
+    public static Transform get() {
+        Transform t;
+        if (m_inactivePool.size() < 1) {
+            m_activePool.add(t = create());
+        } else {
+            m_activePool.add(t = m_inactivePool.remove(0));
+        }
+        t.setCullHint(Spatial.CullHint.Dynamic);
+        return t;
+    }
+    public static boolean dispose(Transform t) {
+        boolean b = m_activePool.remove(t);
+        if (b) {
+            b = m_inactivePool.add(t);
+            t.setCullHint(Spatial.CullHint.Always);
+        }
+        return b;
+    }
+    
+    private static Transform create() {
+        Transform t = new Transform();
+        WorldCreator.createBox(t);
+        return t;
+    }
+}

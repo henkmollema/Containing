5334b9f0cbf2345352eb589cb333e7f224d67ee5
nielscil <niels.bouma@outlook.com>
2015-11-27 13:40:59 +0100

    -Added networking library to android project

---
+++ src/App/app/src/main/java/nhl/containing/managmentinterface/communication/CommunicationProtocol.java
@@ -0,0 +1,15 @@
+package nhl.containing.managmentinterface.communication;
+
+/**
+ * Created by Niels on 27-11-2015.
+ */
+public class CommunicationProtocol extends nhl.containing.networking.protocol.CommunicationProtocol
+{
+
+    @Override
+    public byte[] processInput(byte[] in)
+    {
+
+        return super.processInput(in);
+    }
+}

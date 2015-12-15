33130b4980d58c9bb09f238eae168dc6f3e0a874
Henk Mollema <henkmollema@gmail.com>
2015-12-15 13:38:40 +0100

    Clean-up, again :grin:

--- src/Controller/src/nhl/containing/controller/networking/Server.java
+++ src/Controller/src/nhl/containing/controller/networking/Server.java
@@ -1,10 +1,7 @@
 package nhl.containing.controller.networking;
 
-import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import nhl.containing.controller.Simulator;
 import nhl.containing.networking.messaging.*;
 import nhl.containing.networking.protobuf.ClientIdProto.ClientIdentity;
@@ -15,122 +12,131 @@ import nhl.containing.networking.protocol.CommunicationProtocol;
  *
  * @author Jens
  */
-public class Server implements Runnable {
-
+public class Server implements Runnable
+{
     public static final int PORT = 1337;
-    
     public Simulator simulator;
-    
     private boolean isSimulatorConnected;
     private boolean isAppConnected;
     private boolean shouldRun;
     private ServerSocket serverSocket = null;
     private CommunicationProtocol simCom;
-    
+
     public CommunicationProtocol simCom()
     {
         return simCom;
     }
 
-    public Server(Simulator _simulator) {
+    public Server(Simulator _simulator)
+    {
         simulator = _simulator;
         simCom = new CommunicationProtocol(); //The communication protocol used for the simulator connection
-        
+
     }
-    
+
     public Simulator getSimulator()
     {
         return simulator;
     }
 
-    public boolean isSimulatorConnected() {
+    public boolean isSimulatorConnected()
+    {
         return isSimulatorConnected;
     }
-    
-    public boolean isAppConnected() {
+
+    public boolean isAppConnected()
+    {
         return isAppConnected;
     }
 
-    public void stop() {
+    public void stop()
+    {
         shouldRun = false;
     }
-    
+
     public void onSimDisconnect()
     {
         this.isSimulatorConnected = false;
-        
+
     }
 
     @Override
-    public void run() {
-        if (shouldRun) { //If already running, return..
+    public void run()
+    {
+        if (shouldRun)
+        { //If already running, return..
             return;
         }
-            
+
         shouldRun = true;
 
-        try {
+        try
+        {
             serverSocket = new ServerSocket(PORT);
-        } catch (Exception ex) {
+        }
+        catch (Exception ex)
+        {
             ex.printStackTrace();
         }
-        
-        while(shouldRun)//TODO: Also check for app connection, and start appropriate thread
+
+        while (shouldRun)//TODO: Also check for app connection, and start appropriate thread
         {
             Socket tmpSocket;
             p("Waiting for connection");
-            try{
+            try
+            {
                 tmpSocket = serverSocket.accept();
-                
-                BufferedInputStream input = new BufferedInputStream(tmpSocket.getInputStream());
-                ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
-                OutputStream output = tmpSocket.getOutputStream();
-                
-                //The first message we recieve should be an clientidentity object
+
+                // The first message we recieve should be an clientidentity object
                 byte[] data = StreamHelper.readByteArray(tmpSocket.getInputStream());
                 ClientIdentity idData = ClientIdentity.parseFrom(data);
-                
-                switch(idData.getClientType())
+
+                switch (idData.getClientType())
                 {
                     case SIMULATOR:
-                        if(isSimulatorConnected)
+                        if (isSimulatorConnected)
                         {
-                            tmpSocket.close(); //Accept only one sim connection
+                            // Accept only one sim connection
+                            tmpSocket.close();
                             return;
                         }
 
                         SimHandler simHandler = new SimHandler(this, tmpSocket);
-                        new Thread(simHandler).start(); //Start anonymous thread
-                        
+                        // Start anonymous thread
+                        new Thread(simHandler).start();
+
                         isSimulatorConnected = true;
                         p("Sim connected");
-                        
                         break;
+
                     case APP:
                         isAppConnected = true;
                         p("App connected");
-                        
-                        //Create new appHandler and run it on it's own thread
+
+                        // Create new appHandler and run it on it's own thread
                         AppHandler appHandler = new AppHandler(this, tmpSocket);
                         new Thread(appHandler).start();
-                        
                         break;
                 }
-                
-            
-                
-                
-            }catch(Exception ex){p("Client connection failed"); ex.printStackTrace();}
-            try {
+            }
+            catch (Exception ex)
+            {
+                p("Client connection failed");
+                ex.printStackTrace();
+            }
+            try
+            {
                 Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            catch (InterruptedException ex)
+            {
+                ex.printStackTrace();
             }
         }
-        
     }
-    
-    private static void p(String s) {
+
+    private static void p(String s)
+    {
         System.out.println("Controller: " + s);
     }
 }

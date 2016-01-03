f09e68beec9125740afff3aeeab21cc6bfbdb58e
Dudecake <coen_koomen@hotmail.com>
2016-01-03 20:43:19 +0100

    added background in gui and fiddled with path

--- src/Controller/src/nhl/containing/controller/Main.java
+++ src/Controller/src/nhl/containing/controller/Main.java
@@ -7,20 +7,20 @@ import java.io.File;
 @SuppressWarnings("all")
 public class Main
 {
-
+    /*
     static
     {
-//        try
-//        {
-//            File file = new File(Main.class.getResource("/lib/").getPath(), "Controller.dll");
-//            System.load(file.getAbsolutePath());
-//        }
-//        catch (Exception ex)
-//        {
-//            ex.printStackTrace();
-//            System.exit(1);
-//        }
-    }
+        try
+        {
+            File file = new File(Main.class.getResource("/lib/").getPath(), "Controller.dll");
+            System.load(file.getAbsolutePath());
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }/**/
 
     /**
      * @param args the command line arguments
@@ -32,19 +32,33 @@ public class Main
             @Override
             public void run()
             {
-                //PathFinder.cleanup();
+                try { PathFinder.cleanup(); }
+                catch (Exception ex) {}
                 System.out.println("exit");
             }
         });
 
         run();
-
-//        PathFinder.initPath(new Dimension(25, 25));
-//        int[] path = PathFinder.getPath(0, 130, 5.0f);
-//        for (int i : path)
-//        {
-//            System.out.println("node " + i);
-//        }
+        
+        // TODO find out why pathfinding doesn't return a path when there are obstructions.
+        /*
+        int size = 25;
+        long start = System.currentTimeMillis();
+        PathFinder.initPath(new Dimension(size, size));
+        long end = System.currentTimeMillis();
+        long elapsed = end - start;
+        System.out.println("grid gegenereerd in: " + String.valueOf(elapsed) + " ms");
+        start = System.currentTimeMillis();
+        int[] path = PathFinder.getPath(0, (int)Math.pow(size, 2) - 1, 5.0f);
+        end = System.currentTimeMillis();
+        for (int i : path)
+        {
+            System.out.println("node " + i);
+        }
+        System.out.println("grid gegenereerd in: " + String.valueOf(elapsed) + " ms");
+        System.out.println("pad gegenereerd in: " + String.valueOf(end - start) + " ms");
+        System.out.println("Pad is " + String.valueOf(path.length) + " lang");
+        /**/
     }
 
     /**
